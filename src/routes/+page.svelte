5c6e58d3523dc5fbcc5ed0922832b361856cb917
Maythiwat Chomchuen <maythiwat.chom@gmail.com>
2022-11-27 03:23:48 +0700

    feat: add tailwindcss

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,9 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<div class="bg-slate-900 min-h-screen flex justify-center">
+  <div class="self-center">
+    <h1
+      class="text-4xl sm:text-6xl md:text-8xl font-bold text-transparent bg-clip-text bg-gradient-to-r from-sky-400 to-purple-500"
+    >
+      Hello world!
+    </h1>
+  </div>
+</div>
